813171f39dcaae2d40ae9f04a1eccb8dadaef3ed
Josias Guzman <jguzman@aoiths.org>
2023-08-30 19:12:25 -0400

    refactor

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
+import ms from 'ms';
 import { GameQuery } from '../App';
-import ms from 'ms'
 import APIClient, { FetchResponse } from '../services/api-client';
 import { Platform } from './usePlatforms';
 
