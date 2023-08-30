36b2ebef6809bdd3b96abae445351cea195828c0
Josias Guzman <jguzman@aoiths.org>
2023-08-30 17:12:40 -0400

    Refactor - simplify game query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,9 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { GameQuery } from "../App";
-import APIClient, { FetchResponse } from "../services/api-client";
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { GameQuery } from '../App';
+import APIClient, { FetchResponse } from '../services/api-client';
 import { Platform } from './usePlatforms';
 
-const apiClient = new APIClient<Game>('/games')
+const apiClient = new APIClient<Game>('/games');
 
 export interface Game {
   id: number;
@@ -17,21 +17,20 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) =>
   useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: ({ pageParam = 1}) => 
-      apiClient
-        .getAll({
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-            page: pageParam
-          },
-        }),
-        getNextPageParam: (lastPage, allPages) => {
-          return lastPage.next ?  allPages.length + 1 : undefined;
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam,
         },
-      staleTime: 24 * 60 * 60 * 1000 //24h
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: 24 * 60 * 60 * 1000, //24h
   });
 
 export default useGames;
